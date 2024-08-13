88b95c8f5763ab13ffa8076402ffa44918f31ee1
Victor Koech <vickoch20@gmail.com>
2024-08-13 09:29:31 +0300

    Add environment variable support

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const apiRoutes = require('./routes/api');
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
